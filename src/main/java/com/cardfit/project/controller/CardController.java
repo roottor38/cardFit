985566852bb5236d47f4fd70e0575995b3b8edba
roottor38 <roottor38@gmail.com>
2019-10-17 11:13:05 +0900

    integrate version 0.1 integrate

--- src/main/java/com/cardfit/project/controller/CardController.java
+++ src/main/java/com/cardfit/project/controller/CardController.java
@@ -6,13 +6,18 @@ import org.elasticsearch.search.SearchHits;
 import org.json.simple.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cardfit.project.service.CardService;
 
 
-@RestController
+@Controller
 @Component
 public class CardController {
 	public CardController() {
@@ -27,8 +32,6 @@ public class CardController {
 	// 구현 기능1 : 내 카드 검색
 	@GetMapping("/searchMyCardByCardName")
 	public JSONArray searchMyCardByCardName() throws IOException{
-		JSONArray result = new JSONArray();
-		//result = (JSONArray)hitsResult;
 		return service.cardNameSearch("cardname", "요기요");
 	}
 	
@@ -39,6 +42,24 @@ public class CardController {
 		JSONArray result = service.keywordSearch("이디야");
 		System.out.println(result.getClass());
 		return result;
+	}
+	
+	@RequestMapping("/myCardBenefit")
+	public String myCardBenefit() {
+		System.out.println("@RequestMapping");
+		return "myCardBenefit";
+	}
+
+	@PostMapping("/myCardBenefit")
+	public String myCardBenefit(@RequestParam(value = "bankName") String bankName, @RequestParam(value = "cardName") String cardName, Model model) throws IOException{
+		if (cardName == null || cardName.length() == 0 || cardName.equals("")) {
+			return "myCardBenefit";
+		} else {
+			System.out.println(bankName + "      " + cardName);
+			JSONArray result = service.cardNameSearch("cardname", cardName);
+			model.addAttribute("card", result);
+			return "myCardBenefit";
+		}
 	}
 	
 	
--- src/main/java/controller/EncoreController.java
+++ src/main/java/controller/EncoreController.java
@@ -1,123 +1,123 @@
-package controller;
-
-import java.util.ArrayList;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class EncoreController {
-
-	public EncoreController() {
-		System.out.println("***** EncoreController *****");
-	}
-
-	@RequestMapping("/myCardBenefit")
-	public String myCardBenefit() {
-		return "myCardBenefit";
-	}
-
-	@PostMapping("/myCardBenefit")
-	public String myCardBenefit(@RequestParam(value = "bankName") String bankName,
-			@RequestParam(value = "cardName") String cardName, Model model) {
-		if (cardName == null || cardName.length() == 0 || cardName.equals("")) {
-			return "myCardBenefit";
-		} else {
-			//내카드 혜택보기 검색 로직
-			
-			model.addAttribute("card", //객체저장);
-			return "myCardBenefit";
-		}
-	}
-
-	@RequestMapping("/option")
-	public String option() {
-		return "option";
-	}
-
-	@PostMapping("/option")
-	public String option(@RequestParam(value="movie", required=false) String movie, @RequestParam(value="telecome", required=false) String telecome, @RequestParam(value="traffic", required=false) String traffic,
-			@RequestParam(value="offshop", required=false) String offshop, @RequestParam(value="onshop", required=false) String onshop, @RequestParam(value="food", required=false) String food,
-			@RequestParam(value="cafe", required=false) String cafe, @RequestParam(value="others", required=false) String others, Model model) {
-		//체크 박스로 추천 로직
-		
-		model.addAttribute("data", //객체);
-		return "option";
-	}
-
-	@RequestMapping("/keyword")
-	public String keyword() {
-		return "keyword";
-	}
-
-	@PostMapping("/keyword")
-	public String keyword(@RequestParam(value = "search") String search, Model model) {
-		if (search == null || search.equals("") || search.length() == 0) {
-			return "keyword";
-		} else {
-			//키워드 추천 로직
-			
-			model.addAttribute("data", //객체);
-		}
-		return "keyword";
-	}
-
-	@PostMapping("/cardControl")
-	public String cardControl(@RequestParam(value = "bankName") String bankName,
-			@RequestParam(value = "cardName") String cardName, Model model) {
-		//관리자 카드 검색 로직( 위 내카드 혜택보기랑 같은 로직)
-		
-		model.addAttribute("card", //객체);
-		return "cardControl";
-	}
-
-	@GetMapping("/deleteCard/{bankName}/{cardName}")
-	public String deleteCard(@PathVariable(value = "bankName") String bankName,
-			@PathVariable(value = "cardName") String cardName) {
-		//관리자 은행명하고 카드명으로 카드 삭제 로직
-		
-		
-		return "redirect:/admin.html";
-	}
-	
-	@RequestMapping("/createCard")
-	public String createCard() {
-		return "createCard";
-	}
-	
-	@PostMapping("/createCard")
-	public String createCard(@RequestParam(value = "bankName") String bankName,
-			@RequestParam(value = "cardName") String cardName,
-			@RequestParam(value = "condition") String condition,
-			@RequestParam(value = "benefit") String benefit) {
-		//관리자 카드 추가 로직
-		
-		return "redirect:/admin.html";
-	}
-
-	@RequestMapping("/updateCard/{bankName}/{cardName}")
-	public String updateCard(@PathVariable(value = "bankName") String bankName,
-			@PathVariable(value = "cardName") String cardName, Model model) {
-		//카드 수정을 위한 은행명하고 카드명으로 값 검색 로직
-		
-		
-		model.addAttribute("card", //객체);
-		return "updateCard";
-	}
-	
-	@PostMapping("/updateCard")
-	public String updateCard(@RequestParam(value = "bankName") String bankName,
-			@RequestParam(value = "cardName") String cardName,
-			@RequestParam(value = "condition") String condition,
-			@RequestParam(value = "benefit") String benefit) {
-		//관리자 카드 정보 수정 로직
-		
-		return "redirect:/admin.html";
-	}
-
-}
+//package controller;
+//
+//import java.util.ArrayList;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//public class EncoreController {
+//
+//	public EncoreController() {
+//		System.out.println("***** EncoreController *****");
+//	}
+//
+//	@RequestMapping("/myCardBenefit")
+//	public String myCardBenefit() {
+//		return "myCardBenefit";
+//	}
+//
+//	@PostMapping("/myCardBenefit")
+//	public String myCardBenefit(@RequestParam(value = "bankName") String bankName,
+//			@RequestParam(value = "cardName") String cardName, Model model) {
+//		if (cardName == null || cardName.length() == 0 || cardName.equals("")) {
+//			
+//			return "myCardBenefit";
+//		} else {
+//			
+//			model.addAttribute("card", //객체저장);
+//			return "myCardBenefit";
+//		}
+//	}
+//
+//	@RequestMapping("/option")
+//	public String option() {
+//		return "option";
+//	}
+//
+//	@PostMapping("/option")
+//	public String option(@RequestParam(value="movie", required=false) String movie, @RequestParam(value="telecome", required=false) String telecome, @RequestParam(value="traffic", required=false) String traffic,
+//			@RequestParam(value="offshop", required=false) String offshop, @RequestParam(value="onshop", required=false) String onshop, @RequestParam(value="food", required=false) String food,
+//			@RequestParam(value="cafe", required=false) String cafe, @RequestParam(value="others", required=false) String others, Model model) {
+//		//체크 박스로 추천 로직
+//		
+//		model.addAttribute("data", //객체);
+//		return "option";
+//	}
+//
+//	@RequestMapping("/keyword")
+//	public String keyword() {
+//		return "keyword";
+//	}
+//
+//	@PostMapping("/keyword")
+//	public String keyword(@RequestParam(value = "search") String search, Model model) {
+//		if (search == null || search.equals("") || search.length() == 0) {
+//			return "keyword";
+//		} else {
+//			//키워드 추천 로직
+//			
+//			model.addAttribute("data", //객체);
+//		}
+//		return "keyword";
+//	}
+//
+//	@PostMapping("/cardControl")
+//	public String cardControl(@RequestParam(value = "bankName") String bankName,
+//			@RequestParam(value = "cardName") String cardName, Model model) {
+//		//관리자 카드 검색 로직( 위 내카드 혜택보기랑 같은 로직)
+//		
+//		model.addAttribute("card", //객체);
+//		return "cardControl";
+//	}
+//
+//	@GetMapping("/deleteCard/{bankName}/{cardName}")
+//	public String deleteCard(@PathVariable(value = "bankName") String bankName,
+//			@PathVariable(value = "cardName") String cardName) {
+//		//관리자 은행명하고 카드명으로 카드 삭제 로직
+//		
+//		
+//		return "redirect:/admin.html";
+//	}
+//	
+//	@RequestMapping("/createCard")
+//	public String createCard() {
+//		return "createCard";
+//	}
+//	
+//	@PostMapping("/createCard")
+//	public String createCard(@RequestParam(value = "bankName") String bankName,
+//			@RequestParam(value = "cardName") String cardName,
+//			@RequestParam(value = "condition") String condition,
+//			@RequestParam(value = "benefit") String benefit) {
+//		//관리자 카드 추가 로직
+//		
+//		return "redirect:/admin.html";
+//	}
+//
+//	@RequestMapping("/updateCard/{bankName}/{cardName}")
+//	public String updateCard(@PathVariable(value = "bankName") String bankName,
+//			@PathVariable(value = "cardName") String cardName, Model model) {
+//		//카드 수정을 위한 은행명하고 카드명으로 값 검색 로직
+//		
+//		
+//		model.addAttribute("card", //객체);
+//		return "updateCard";
+//	}
+//	
+//	@PostMapping("/updateCard")
+//	public String updateCard(@RequestParam(value = "bankName") String bankName,
+//			@RequestParam(value = "cardName") String cardName,
+//			@RequestParam(value = "condition") String condition,
+//			@RequestParam(value = "benefit") String benefit) {
+//		//관리자 카드 정보 수정 로직
+//		
+//		return "redirect:/admin.html";
+//	}
+//
+//}
